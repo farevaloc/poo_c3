897caacbd360ff092bef284500ea4dbbee1a3da1
farevaloc <felipearevalo87@gmail.com>
2024-06-18 18:22:45 -0500

    commit app aichat

--- chatbot_c3/settings.py
+++ chatbot_c3/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # App propias
+    'aichat',
+
 ]
 
 MIDDLEWARE = [
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'chatbot_c3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat_c3',
+        'USER': 'chat_c3_user',
+        'PASSWORD': 'emelec',
+        'HOST': '127.0.0.1',
+        'PORT': '5436',
     }
 }
 
@@ -104,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
