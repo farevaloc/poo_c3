eb333956f2fc7f81e605aff9b209039844a57ec8
farevaloc <felipearevalo87@gmail.com>
2024-06-25 20:06:13 -0500

    commit app aichat parte 1

--- aichat/views.py
+++ aichat/views.py
@@ -1,12 +1,17 @@
+from django.contrib import messages
+from django.contrib.auth import login
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
+from aichat.forms import DatosUserCreacionForm
 
 
 # Create your views here.
 
 class RegistroView(CreateView):
     form_class = DatosUserCreacionForm
-    template_name = 'register.html'
+    template_name = 'registro.html'
     success_url = reverse_lazy('chat')
 
     def form_valid(self, form):
--- chatbot_c3/settings.py
+++ chatbot_c3/settings.py
@@ -136,3 +136,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'aichat.DatosUsuario'
+
+LOGIN_URL = 'login'
